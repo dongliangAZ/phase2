50284e291ad9d2c9c3ec8cb4b6a399a1d020c80c
Dong Liang <hermosbird@gmail.com>
2018-02-07 21:17:06 -0700

    check_io

--- phase2.c
+++ phase2.c
@@ -313,12 +313,17 @@ int MboxReceive(int mbox_id, void *msg_ptr, int msg_size)
 
 /* ------------------------------------------------------------------------
    Name - check_io
-   Purpose - I don't know
-   Parameters - none that I know of
-   Returns - duck if I know
+   Purpose - Determine if there any processes blocked on any of the
+             interrupt mailboxes.
+   Returns - 1 if one (or more) processes are blocked; 0 otherwise
    Side Effects - none.
-   ----------------------------------------------------------------------- */
-void check_io()
-{
 
-}
+   Note: Do nothing with this function until you have successfully completed
+   work on the interrupt handlers and their associated mailboxes.
+   ------------------------------------------------------------------------ */
+int check_io(void)
+{
+    if (DEBUG2 && debugflag2)
+        USLOSS_Console("check_io(): called\n");
+    return 0;
+} /* check_io */
